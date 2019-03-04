47b95f799bd9a587ad386d2da0d6b3580b58c89a
Kieran McCool <kieranmccool332@gmail.com>
2019-03-04 23:04:23 +0000

    scrape.py now functional. Included datafiles because why not, never know when you might need a the list of every npc name in the Elder Scrolls.

--- scrape.py
+++ scrape.py
@@ -5,36 +5,48 @@
 #              for all names in the Elder Scrolls universe (so far) by race and gender.
 #              It outputs files for each into the ./DataSets directory.
 
-import requests
+import os.path
 from re import sub
+import requests
 from bs4 import BeautifulSoup, Tag
 
-url = 'https://en.uesp.net/wiki/Lore:%s_Names'
-base_file_name = '%s_%s_names.txt'
+races = ['Altmer', 'Argonian', 'Bosmer', 'Breton', 'Dunmer',
+            'Imperial', 'Khajiit', 'Nord', 'Orc', 'Redguard']
 
-def getPage():
-    race = 'Altmer'
+url = 'https://en.uesp.net/wiki/Lore:%s_Names'
+base_file_name = './DataFiles/%s_%s_names.txt'
+
+def getPage(race):
     html = requests.get(url % (race)).text
     soup = BeautifulSoup(html, 'html.parser')
     return soup
 
-def getNames(soup, baseFileName):
+def getNames(soup, race):
     headers = soup.findAll('h3')
     maleNameHeaders = headers[1:10]
     femaleNameHeaders = headers[11:19]
+
+    # TODO: family names need a different parsing method.
     # familyNameHeaders = headers[20:27]
 
-    for gender in [maleNameHeaders, femaleNameHeaders]:
-        for game in gender:
-            raw_text = game.findNextSibling('p').text
-            stripped_text = sub(r'\([\d, .]*\)', '', raw_text)
-            stripped_text = sub(r'(\d)*x:', '', stripped_text)
-            stripped_text = '\n'.join(set([s.strip() for s in stripped_text.split(',')]))
-            print(stripped_text)
+    for i, gender in enumerate([maleNameHeaders, femaleNameHeaders]):
+        with open(base_file_name % ( ['Male', 'Female'][i], race), 'w') as output:
+            for game in gender:
+                raw_text = game.findNextSibling('p').text.replace('\n', ', ')
+                stripped_text = sub(r'\([\d, .]*\)', '', raw_text)
+                stripped_text = sub(r'^(.)*:', '', stripped_text)
+                stripped_text = '\n'.join(set([s.strip() 
+                    for s in stripped_text.split(',') if s.strip() != '']))
+                output.write(stripped_text + '\n')
 
 def main():
-    soup = getPage()
-    getNames(soup, )
+
+    if not os.path.exists('./DataFiles'):
+        os.mkdir('./DataFiles')
+
+    for race in races:
+        soup = getPage(race)
+        getNames(soup, race)
 
 if __name__ == "__main__":
     main()
